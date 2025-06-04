6769a2245af9bbecccccf460c93a84044b2d9291
Mafe Duarte A <mfda.1396@gmail.com>
2025-06-04 11:30:11 +0000

    carta aleatoria

--- src/app.js
+++ src/app.js
@@ -5,7 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
+
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const palos = [
+    { simbolo: "♥" },
+    { simbolo: "♦" },
+    { simbolo: "♣" },
+    { simbolo: "♠" }
+  ];
+
+  const valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9","J", "Q", "K"];
+
+  const palo = palos[Math.floor(Math.random() * palos.length)];
+  const valor = valores[Math.floor(Math.random() * valores.length)];
+
+  let color;
+  if (palo.simbolo === "♥" || palo.simbolo === "♦") {
+    color = "red";
+  } else {
+    color = "black";
+  }
+
+  const topElemento = document.getElementById("top");
+  const numberElemento = document.getElementById("number");
+  const bottomElemento = document.getElementById("bottom");
+
+  topElemento.innerHTML = palo.simbolo;
+  topElemento.style.color = color;
+
+  numberElemento.innerHTML = valor;
+  numberElemento.style.color = color;
+
+  bottomElemento.innerHTML = palo.simbolo;
+  bottomElemento.style.color = color;
+  
 };
+
+  
